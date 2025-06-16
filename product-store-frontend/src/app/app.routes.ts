fc71bf835e606b9d742e9f04c73d51fbf1dc90e8
ytrigui <ytrigui9@gmail.com>
2025-06-16 16:07:14 +0100

    version_after_add_dashbord

--- product-store-frontend/src/app/app.routes.ts
+++ product-store-frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { WelcomeComponent }   from './components/welcome/welcome.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductFormComponent } from './components/product-form/product-form.component';
 import { ProductEditComponent } from './components/product-edit/product-edit.component';
+import {AnalyticsDashboardComponent} from './components/analytics-dashboard/analytics-dashboard.component';
 
 export const routes: Routes = [
   { path: '',          component: WelcomeComponent },
   { path: 'products',  component: ProductListComponent },
   { path: 'add',       component: ProductFormComponent },
-  { path: 'edit/:id',  component: ProductEditComponent }
+  { path: 'edit/:id',  component: ProductEditComponent },
+  { path: 'dashboard',  component: AnalyticsDashboardComponent }
 ];
---
+++ product-store-frontend/src/app/components/analytics-dashboard/analytics-dashboard.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product.model';
+import {NgIf} from '@angular/common';
+
+@Component({
+  selector: 'app-analytics-dashboard',
+  templateUrl: './analytics-dashboard.component.html',
+  imports: [
+    NgIf
+  ],
+  styleUrls: ['./analytics-dashboard.component.scss']
+})
+export class AnalyticsDashboardComponent implements OnInit {
+  totalProducts = 0;
+  lastAddedProduct: Product | null = null;
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.getAll().subscribe(products => {
+      this.totalProducts = products.length;
+      this.lastAddedProduct = products[products.length - 1]; // Assuming latest is last
+    });
+  }
+}
