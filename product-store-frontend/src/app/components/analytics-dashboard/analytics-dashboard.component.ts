4b6f1cb43919346dfad21a69b51cba8ca56cca0f
ytrigui <ytrigui9@gmail.com>
2025-06-18 09:45:06 +0100

    final version 2

--- product-store-frontend/src/app/components/analytics-dashboard/analytics-dashboard.component.ts
+++ product-store-frontend/src/app/components/analytics-dashboard/analytics-dashboard.component.ts
@@ -14,13 +14,27 @@ import {NgIf} from '@angular/common';
 export class AnalyticsDashboardComponent implements OnInit {
   totalProducts = 0;
   lastAddedProduct: Product | null = null;
+  allProducts: Product[] = [];
+  maxPriceProduct: Product | null = null;
+  minPriceProduct: Product | null = null;
 
   constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
     this.productService.getAll().subscribe(products => {
       this.totalProducts = products.length;
-      this.lastAddedProduct = products[products.length - 1]; // Assuming latest is last
+      this.lastAddedProduct = products[products.length - 1];// Assuming latest is last
+
+      this.maxPriceProduct = products.reduce((max, curr) =>
+        (curr.price > (max.price || 0)) ? curr : max, products[0]
+      );
+
+      // Min price product
+      this.minPriceProduct = products.reduce((min, curr) =>
+        (curr.price < (min.price || Infinity)) ? curr : min, products[0]
+      );
     });
+
+
   }
 }
